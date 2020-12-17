e15c0950effb8723754a380054c53343d3d73fd8
dhany007 <dhanyaritonang@gmail.com>
2020-12-17 18:15:09 +0700

    change port

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const bodyParser = require('body-parser')
 const routerNav = require('./src/index')
 const db = require('./src/models/index')
 
+require('dotenv').config()
+
 const app = express()
 
-const port = 10010
+const port = process.env.PORT || 10010
 app.listen(port, () => console.log('Server listening on port', port))
 
 app.use(bodyParser.json())
